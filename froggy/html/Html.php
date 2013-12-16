07c77cbc119d0c62bc5a53c2e8df4bc9208f7581
Muhammad Fariz Arya <bloblo42@gmail.com>
2013-12-16 19:02:14 +0700

    fix html class

--- froggy/html/Html.php
+++ froggy/html/Html.php
@@ -1,6 +1,7 @@
 <?php
 
 class HTML {
+
 	public static function anchor($text,$link,$option=null){
 		echo "<a href=\"".$link."\">".$text."</a>";
 	}
@@ -28,8 +29,8 @@ class HTML {
 	public static function textbox($name,$value,$attr=array()){
 
 		$var='';
-		foreach ($attr as $key => $value) {
-			$var.="$key = \"$value\"";
+		foreach ($attr as $key => $val) {
+			$var.="$key=\"$val\"";
 		}
 
 		echo "<input type=\"text\" name=\"".$name."\" value=\"".$value."\" ".$var." />";
@@ -37,8 +38,8 @@ class HTML {
 
 	public static function textarea($name,$value,$attr=array()){
 		$var='';
-		foreach ($attr as $key => $value) {
-			$var.="$key = \"$value\"";
+		foreach ($attr as $key => $val) {
+			$var.="$key = \"$val\"";
 		}
 
 		echo "<textarea name=\"".$name."\" ".$var." >".$value."</textarea>\n";
@@ -60,32 +61,59 @@ class HTML {
 
 	public static function button($name,$value,$attr=array()){
 		$var='';
-		foreach ($attr as $key => $value) {
-			$var.="$key = \"$value\"";
+		foreach ($attr as $key => $val) {
+			$var.="$key = \"$val\"";
 		}
 
 		echo "<input type=\"button\" name=\"".$name."\" value=\"".$value."\" ".$var." />";
 	}
 
-	public static function label($value,$attr){
+	public static function label($value,$attr=array()){
 		$var='';
-		foreach ($attr as $key => $value) {
-			$var.="$key = \"$value\"";
+		foreach ($attr as $key => $val) {
+			$var.="$key = \"$val\"";
 		}
 
-		echo "<label>".$value."</label>";
+		echo "<label $var>".$value."</label>";
 	}
 
-	public static function submit($value,$attr){
+	public static function submit($value,$attr=array()){
 		$var='';
-		foreach ($attr as $key => $value) {
-			$var.="$key = \"$value\"";
+		foreach ($attr as $key => $val) {
+			$var.="$key = \"$val\"";
 		}
 
 		echo "<input type=\"submit\" value=\"".$value."\" ".$var." />";
 	}
 
-	public static function form($action,$method,$attr){
+	public static function reset($value,$attr=array()){
+		$var='';
+		foreach ($attr as $key => $val) {
+			$var.="$key = \"$val\"";
+		}
+
+		echo "<input type=\"reset\" value=\"".$value."\" ".$var." />";
+	}
+
+	public static function input($type,$name,$value,$attr=array()){
+		$var='';
+		foreach ($attr as $key => $val) {
+			$var.="$key = \"$val\"";
+		}
+
+		echo "<input type=\"$type\" name=\"$name\" value=\"".$value."\" ".$var." />";
+	}
+
+	public static function password($name,$value,$attr=array()){
+		$var='';
+		foreach ($attr as $key => $val) {
+			$var.="$key = \"$val\"";
+		}
+
+		echo "<input type=\"password\" value=\"".$value."\" ".$var." />";
+	}
+
+	public static function form($action,$method,$attr=array()){
 		$var='';
 		foreach ($attr as $key => $value) {
 			$var.="$key = \"$value\"";
@@ -98,11 +126,61 @@ class HTML {
 		echo "</form>";
 	}
 
-	public static function br(){
-		echo "<br />";
+	public static function checkbox($name,$value=array(),$attr=array(),$order=false){
+
+		$par='';
+		if($order){
+			$par='<br />';
+		}
+		
+		foreach ($value as $keys => $val) {
+
+			$var='';
+
+			foreach ($attr as $key => $value) {
+				$var.=$key." = \"$value\"";
+			}
+
+			echo "<input type=\"checkbox\" name=\"".$name."[]\" ".$var." />".$keys." ".$par;			
+		}
+
 	}
 
-	public static function hr(){
-		echo "<hr />";
+	public static function radio($name,$value=array(),$attr=array(),$order=false){
+		$par='';
+		if($order){
+			$par='<br />';
+		}
+		
+		foreach ($value as $keys => $val) {
+
+			$var='';
+
+			foreach ($attr as $key => $value) {
+				$var.=$key." = \"".$value."\"";
+			}
+
+			echo "<input type=\"radio\" name=\"".$name."\" ".$var." />".$keys." ".$par;			
+		}
 	}
+
+	public static function br(){
+            echo "<br />";
+    }
+
+    public static function hr(){
+            echo "<hr />";
+    }
+
+    public static function image($path,$width,$height,$attr=array()){
+    	$var='';
+		foreach ($attr as $key => $value) {
+			$var.="$key = \"$value\"";
+		}
+    	echo "<img src=\"$path\" width=\"$width\" height=\"$height\" $var />";
+    }
+
+    public static function favicon($path){
+    	echo "<link rel=\"shorcut icon\" href=\"$path>\"";
+    }
 }
