aa931bcedfca4b0978ef64b6c4c033de34ee2aa3
Muhammad Fariz Arya <bloblo42@gmail.com>
2013-12-20 20:35:53 +0700

    fix path class

--- froggy/path/Path.php
+++ froggy/path/Path.php
@@ -10,8 +10,13 @@ class Path {
 		return Configuration::$BASE_PATH . 'public/';
 	}
 
-	public static function full_path(){
+	public static function current_path(){
 		return "http" . (($_SERVER['SERVER_PORT']==443) ? "s://" : "://") . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+	}
+
+	public static function to_path(){
+		$req=explode("/", $_SERVER['REQUEST_URI']);
+		return  "http" . (($_SERVER['SERVER_PORT']==443) ? "s://" : "://") . $_SERVER['HTTP_HOST'] . '/'.$req[1];		
 	}
 
 }
--- froggy/path/Redirect.php
+++ froggy/path/Redirect.php
@@ -1,11 +1,15 @@
 <?php
 
 class Redirect {
-	public static function to($link){
-		echo "<meta http-equive=\"\" content=\"0;URL=".$link."\">";
+	public static function to($link,$msg=array()){
+		foreach ($msg as $key => $value) {
+			Session::set($key,$value);
+		}
+		echo "<meta http-equiv=\"refresh\" content=\"0;URL=".Path::to_path().$link."\">";
+
 	}
 
 	public static function toWhen($link,$time){
-		echo "<meta http-equive=\"\" content=\"".$time.";URL=".$link."\">";
+		echo "<meta http-equiv=\"refresh\" content=\"".$time.";URL=".Path::to_path().$link."\">";
 	}
 }
